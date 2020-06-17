e13138c6fabe6a5e09e321c7dd5dca24a862a0d4
Cédric Courtaud <cedric.courtaud@gmail.com>
2020-06-17 17:32:31 +0200

    Split EventQueue and EventDispatcher

--- internal/EventQueue.go
+++ internal/EventQueue.go
@@ -1,1 +1,29 @@
 package internal
+
+type EventQueue struct {
+	Queue chan Event
+	Done chan bool
+	handler *EventHandler
+}
+
+func NewEventQueue(handler *EventHandler) *EventQueue {
+	return &EventQueue{handler: handler, Queue: make(chan Event), Done: make(chan bool)}
+}
+
+func (e *EventQueue) Start() {
+	go WaitForEvent(*e.handler, e.Queue, e.Done)
+}
+
+func (e *EventQueue) Stop() {
+	close(e.Queue)
+}
+
+func (e *EventQueue) HandleAccess(access *Access) error {
+	e.Queue <- access
+	return nil
+}
+
+func (e *EventQueue) HandleCheckpoint(checkpoint *Checkpoint) error {
+	e.Queue <- checkpoint
+	return nil
+}
